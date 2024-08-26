2006e286b529abc398d320299e6e7f1ba83b8f4b
mamun700263 <mamun700263@gmail.com>
2024-08-26 21:53:08 +0600

    he

--- Assaignment5/settings.py
+++ Assaignment5/settings.py
@@ -30,8 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://assaignment-5.onrender.com','https://*.127.0.0.1']
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://assaignment-5.onrender.com','https://*.127.0.0.1'
+]
 # Application definition
 
 INSTALLED_APPS = [

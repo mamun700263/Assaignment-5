e944c8dfc6248b71b33e9af6bd961c98355d9091
mamun700263 <mamun700263@gmail.com>
2024-08-26 20:23:46 +0600

    database added

--- Assaignment5/settings.py
+++ Assaignment5/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import environ
 env = environ.Env()
 environ.Env.read_env()
+import dj_database_url
 ...
 from pathlib import Path
 
@@ -92,16 +93,34 @@ WSGI_APPLICATION = 'Assaignment5.wsgi.application'
 
 # Your secret key
 SECRET_KEY = env("SECRET_KEY")
+# DATABASES = {
+#     'default': {
+#     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#     'NAME': env("DB_NAME"),
+#     'USER': env("DB_USER"),
+#     'PASSWORD': env("DB_PASSWORD"),
+#     'HOST': env("DB_HOST"),
+#     'PORT': env("DB_PORT"),
+#     }
+# }
+
+
+
+
+
+
+# Replace the SQLite DATABASES configuration with PostgreSQL:
 DATABASES = {
-    'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': env("DB_NAME"),
-    'USER': env("DB_USER"),
-    'PASSWORD': env("DB_PASSWORD"),
-    'HOST': env("DB_HOST"),
-    'PORT': env("DB_PORT"),
-    }
-}
+    'default': dj_database_url.config(   
+    # Replace this value with your local database's connection string.    
+    default='postgresql://assaignment5_database_user:y0jotVPK2XpjMxDz39bLZwBSOm4h4IAK@dpg-cr68usjtq21c73bbgot0-a.oregon-postgres.render.com/assaignment5_database',
+    )}
+
+
+
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
